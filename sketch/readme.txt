90d71c9243e5b31b392976a5df946c710d54d3ac
marineata <41453891+marineata@users.noreply.github.com>
2018-07-20 01:59:14 +0300

    Add files via upload

---
+++ sketch/readme.txt
@@ -0,0 +1,6 @@
+In fisierul readme.txt o sa las lista de piese folosite:
+-4 servomecanisme
+-arduino nano
+-gheara si constructa metalica realizate din duraluminiu pe cnc si anodizate
+-potentiometrele de control si calibrare
+-sursa de alimentare de 5v cc 
---
+++ sketch/sketch_jul20a.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo gheara;
+
+int pot1 = 0;
+int pot2 = 1;
+int pot3 = 2;
+int pot4 = 3;
+int val1; 
+int val2; 
+int val3; 
+int val4
+; 
+void setup() {
+ servo1.attach(6);
+ servo2.attach(7);
+ servo3.attach(8);
+ gheara.attach(9);
+
+}
+
+void loop() {
+   val1 = analogRead(pot1);
+    val1 = map(val1, 0, 1023, 0, 180);
+    servo1.write(val1);                 
+
+
+     val2 = analogRead(pot2);
+      val2 = map(val2, 0, 1023, 0, 180);
+       servo2.write(val2);                 
+
+
+        val3 = analogRead(pot3);
+         val3 = map(val3, 0, 1023, 0, 180);
+          servo3.write(val3);                  
+
+
+          val4 = analogRead(pot4);
+            val4 = map(val4, 0, 1023, 0, 180);
+            gheara.write(val4);                  
+             delay(50);         
+}
